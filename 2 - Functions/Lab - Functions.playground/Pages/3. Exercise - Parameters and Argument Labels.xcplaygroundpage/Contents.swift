6e65b8c93e159bb77986393741b3003f3a99c816
Anderson <112644138+iskowan@users.noreply.github.com>
2023-02-16 11:57:58 -0500

    Commit 2

--- 2 - Functions/Lab - Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
+++ 2 - Functions/Lab - Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
@@ -10,12 +10,14 @@ func introduction(_ name: String, _ home: String,_ age: Int) {
 
 introduction("Mary", "Califorina", 32)
 //:  Write a function called `almostAddition` that takes two `Int` arguments. The first argument should not require an argument label. The function should add the two arguments together, subtract 2, then print the result. Call the function and observe the printout.
-func almostAddition(_ firstInt: Int, secondInt: Int) -> Int{
-    almostAddition(<#T##firstInt: Int##Int#>, secondInt: <#T##Int#>)
+func almostAddition(_ firstInt: Int, secondInt: Int){
+    print(firstInt + secondInt - 2)
 }
-
+almostAddition(5, secondInt: 7)
 //:  Write a function called `multiply` that takes two `Double` arguments. The function should multiply the two arguments and print the result. The first argument should not require a label, and the second argument should have an external label, `by`, that differs from the internal label. Call the function and observe the printout.
-
+func multiply(_doubleOne: Double, doubleTwo: Double){
+    
+}
 
 /*:
 [Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Progress Updates](@next)
