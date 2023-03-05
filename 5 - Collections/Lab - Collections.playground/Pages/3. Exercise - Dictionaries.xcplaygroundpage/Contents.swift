1e941cd1b128c3d2985ef1378fb10c4d2f387c30
iskowan <112644138+iskowan@users.noreply.github.com>
2023-03-05 18:00:52 -0500

    Commit 13

--- 5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ 5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -16,10 +16,12 @@ print(daysOfMonth)
 //:  Use if-let syntax to retrieve the number of days under "January." If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
 if let janValue = daysOfMonth["January"]{
     print("January has \(daysOfMonth["January"]) days")
+} else {
+    print("I could not find a value in janValue")
 }
-
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors." Print the resulting dictionary.
-
+var shapesArray: [String:[String]]
+var colorArray: [String: [String]]
 
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
 
